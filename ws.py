914533c2652ac9cb3a85a7a1fcaa8ae73ae301bb
Victor Otieno Omondi <vickz84259@gmail.com>
2019-01-17 00:50:15 +0300

    Refactor ws.py
    This change enables the different websocket callbacks to emit signals since
    they now belong within the QThread.
    
    Removed get_authorization_header since network details are now in network.py
    Switched from using league.get_connection_details to the DataStore
    version since the latter caches the credentials.
    
    Signed-off-by: Victor Otieno Omondi <vickz84259@gmail.com>

--- ws.py
+++ ws.py
@@ -1,39 +1,13 @@
-import base64
 import json
 import ssl
 
 import websocket
 import PySide2.QtCore as QtCore
 
-import league
+from data_manager import DataStore
+import network
 
 file = None
-
-
-def on_message(ws, message):
-    if message:
-        print(json.loads(message))
-
-
-def on_error(ws, error):
-    pass
-
-
-def on_close(ws):
-    pass
-
-
-def on_open(ws):
-    ws.send(json.dumps([5, 'OnJsonApiEvent']))
-
-
-def get_authorization_header(token):
-    # Creating the authorization header
-    auth = f'riot:{token}'.encode()
-    b64encoded = base64.b64encode(auth).decode('ascii')
-    authorization_header = f'Basic {b64encoded}'
-
-    return authorization_header
 
 
 class WebSocketThread(QtCore.QThread):
@@ -45,16 +19,29 @@ class WebSocketThread(QtCore.QThread):
         self.ws.close()
         self.wait()
 
+    def on_message(self, message):
+        if message:
+            print(json.loads(message))
+
+    def on_error(self, error):
+        pass
+
+    def on_close(self):
+        pass
+
+    def on_open(self):
+        self.ws.send(json.dumps([5, 'OnJsonApiEvent']))
+
     def run(self):
-        token, port = league.get_connection_details()
+        token, port = DataStore.get_connection_details()
 
         self.ws = websocket.WebSocketApp(
             f"wss://127.0.0.1:{port}/",
-            header={'Authorization': get_authorization_header(token)},
-            on_open=on_open,
-            on_message=on_message,
-            on_error=on_error,
-            on_close=on_close)
+            header={'Authorization': network.get_auth_header(token)},
+            on_open=self.on_open,
+            on_message=self.on_message,
+            on_error=self.on_error,
+            on_close=self.on_close)
 
         self.ws.run_forever(
             sslopt={'cert_reqs': ssl.CERT_NONE}, suppress_origin=True)
