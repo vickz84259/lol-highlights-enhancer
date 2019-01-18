e54ed6c8afb8f1b1e0d71625d03d5105d8eed20d
Victor Otieno Omondi <vickz84259@gmail.com>
2019-01-18 09:52:51 +0300

    Refactor and add retrieving highlights functionality
    The refactoring was necessary in order to have a more cleaner approach
    on how DataStore retrieves and saves data.
    Also, it reduces the amount of redundancy
    
    Signed-off-by: Victor Otieno Omondi <vickz84259@gmail.com>

--- data_manager.py
+++ data_manager.py
@@ -1,32 +1,63 @@
+import re
+
 from ruamel.yaml import YAML
 
 import league
 import network
 
 PREFERENCES_PATH = 'resources\\user_preferences.yaml'
+HIGHLIGHTS_PATH = 'highlights.yaml'
 
 
 class DataStore():
-    preferences = None
     connection_details = None
+
+    __internal_data = {
+        'preferences': {'file_name': PREFERENCES_PATH, 'data': None},
+        'highlights': {'file_name': HIGHLIGHTS_PATH, 'data': None}}
+
+    @classmethod
+    def __save(cls, data, type, to_file=False):
+        file_name = cls.__internal_data[type]['file_name']
+        if to_file:
+            with open(file_name, 'w') as file:
+                yaml = YAML(typ='safe')
+                yaml.dump(data, file)
+
+        cls.__internal_data[type]['data'] = data
+
+    @classmethod
+    def __get(cls, type):
+        file_name = cls.__internal_data[type]['file_name']
+        data = cls.__internal_data[type]['data']
+
+        if data is None:
+            with open(file_name) as file:
+                yaml = YAML(typ='safe')
+                cls.__internal_data[type]['data'] = yaml.load(file)
+
+        return cls.__internal_data[type]['data']
 
     @classmethod
     def get_preferences(cls):
-        if cls.preferences is None:
-            with open(PREFERENCES_PATH) as preferences_file:
-                yaml = YAML(typ='safe')
-                cls.preferences = yaml.load(preferences_file)
+        return cls.__get('preferences')
 
-        return cls.preferences
+    @classmethod
+    def get_highlights(cls):
+        return cls.__get('highlights')
 
     @classmethod
     def save_preferences(cls, data, to_file=False):
-        if to_file:
-            with open(PREFERENCES_PATH, 'w') as preferences_file:
-                yaml = YAML(typ='safe')
-                yaml.dump(data, preferences_file)
+        cls.__save(data, 'preferences', to_file)
 
-        cls.preferences = data
+    @classmethod
+    def save_highlights(cls, data, to_file=False):
+        cls.__save(data, 'highlights', to_file)
+
+    @classmethod
+    def setup(cls):
+        cls.setup_preferences()
+        cls.setup_highlights()
 
     @classmethod
     def setup_preferences(cls):
@@ -45,6 +76,23 @@ class DataStore():
         preferences['lol-highlights-folder'].append(highlights_folder)
 
         cls.save_preferences(preferences, to_file=True)
+
+    @classmethod
+    def setup_highlights(cls):
+        highlights = network.request('/lol-highlights/v1/highlights', 'POST')
+
+        for highlight in highlights:
+            name = highlight['name']
+            regex = re.match(r'\d+-\d+_(\w{2,3}\d?)-(\d+)_\d{2}', name)
+
+            if regex is not None:
+                highlight['region'] = regex.group(1)
+                highlight['match_id'] = regex.group(2)
+            else:
+                highlight['region'] = None
+                highlight['match_id'] = None
+
+        cls.save_highlights(highlights, to_file=True)
 
     @classmethod
     def get_connection_details(cls):
